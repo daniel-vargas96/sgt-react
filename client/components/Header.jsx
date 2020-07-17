68f96ce50d052fc9383df05b6f4765015ed21502
Daniel Vargas <dannyvargashb@gmail.com>
2020-07-17 14:36:43 -0700

    User can view avg grade

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
-class Header extends React.Component {
+function Header(props) {
 
-  render() {
     return (
-      <h1 className="ml-2">Student Grade Table</h1>
+      <div className="header-container">
+        <h1 className="ml-5">Student Grade Table</h1>
+        <div className="average-container ml-3">
+          <label>Average Grade</label>
+          <h3 className="average ml-3 pl-3 pr-3">{props.average}</h3>
+        </div>
+      </div>
+
     );
-  }
 }
 
 export default Header;
--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,6 +6,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { grades: [] };
+    this.getAverageGrade = this.getAverageGrade.bind(this);
   }
 
   componentDidMount() {
@@ -14,11 +15,23 @@ class App extends React.Component {
       .then(data => this.setState({ grades: data }));
   }
 
+  getAverageGrade() {
+    const grades = this.state.grades;
+    let sum = 0;
+    let newAverage;
+    for (let i = 0; i < grades.length; i++) {
+      sum += parseInt(grades[i].grade, 10);
+    }
+    newAverage = Math.trunc(sum / grades.length);
+    return newAverage.toString();
+  }
+
   render() {
+    const average = this.getAverageGrade();
     return (
       <>
-        <Header />
-        <GradeTable grades={this.state.grades}/>
+        <Header average={average} />
+        <GradeTable grades={this.state.grades} />
       </>
     );
   }
